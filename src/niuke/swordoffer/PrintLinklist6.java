b08bf733ef2b6ddce77ab179900cb9214458b3fd
windows10 <279956327@qq.com>
2018-05-04 09:42:04 +0800

    swordOffer 6 print list from tail to head done

--- src/niuke/swordoffer/PrintLinklist6.java
+++ src/niuke/swordoffer/PrintLinklist6.java
@@ -1,6 +1,7 @@
 package niuke.swordoffer;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * test
@@ -11,7 +12,43 @@ import java.util.ArrayList;
 public class PrintLinklist6 {
 
     public static void main(String[] args) {
-        ArrayList<Integer> res = new ArrayList<>();
+        ArrayList<Integer> arraRes = new ArrayList<>();
+        Stack<Integer> res = new Stack<>();
+        res.isEmpty();
+        res.pop();
+//        arraRes.
+
 //        res.
+    }
+
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+
+        ArrayList<Integer> arrayRes = new ArrayList<>();
+        Stack<Integer> res = new Stack<>();
+        if(listNode == null){
+            return arrayRes;
+        }
+
+        res.push(listNode.val);
+        while(listNode.next != null){
+            listNode = listNode.next;
+            res.push(listNode.val);
+        }
+
+        while(!res.isEmpty()){
+            arrayRes.add(res.pop());
+        }
+        return arrayRes;
+
+    }
+}
+
+
+
+class ListNode {
+    int val;
+    ListNode next = null;
+    ListNode(int val) {
+        this.val = val;
     }
 }
