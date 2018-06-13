1fbaa64b5fcf72177abbb83ceecea0200278b3f4
windows10 <279956327@qq.com>
2018-06-13 09:30:47 +0800

    swod offer 26 subBinary tree done

--- src/niuke/swordoffer/SubBinaryTree.java
+++ src/niuke/swordoffer/SubBinaryTree.java
@@ -12,36 +12,46 @@ public class SubBinaryTree {
     public static void main(String[] args) {
         SubBinaryTree subBinaryTree = new SubBinaryTree();
 
-        TreeNode root1 = new TreeNode(0);
-        TreeNode root2 = new TreeNode(1);
-        subBinaryTree.hasSubtree(root1, root2);
+        char[] array1 = {8,8,7,9,2,'#','#','#','#',4,7};
+//        char[] array1 = {8,8,7,9,2,};
+        char[] array2 = {8,9,2};
+        TreeNode root1 = TreeNode.constructTree(array1);
+        TreeNode root2 = TreeNode.constructTree(array2);
 
-
+        System.out.println(subBinaryTree.HasSubtree(root1, root2));
     }
 
 
-    public boolean hasSubtree(TreeNode root1, TreeNode root2){
-        if(root1 == null && root2 == null){
-            return true;
+    public boolean HasSubtree(TreeNode fatherRoot, TreeNode childRoot) {
+        boolean result = false;
+        if (fatherRoot != null && childRoot != null) {
+            if (fatherRoot.val == childRoot.val) {
+                result = isSameTree(fatherRoot, childRoot);
+            }
+            if (!result) {
+                result = HasSubtree(fatherRoot.left, childRoot);
+            }
+            if (!result) {
+                result = HasSubtree(fatherRoot.right, childRoot);
+            }
         }
-        if(root1 == null || root2 == null) {
-            return false;
-        }
-        if(root1.val == root2.val) {
-            return isSameTree(root1, root2);
-        }
-        return hasSubtree(root1.right, root2.right) || hasSubtree(root1.left, root2.left);
+        return result;
     }
-
 
     private boolean isSameTree(TreeNode root1, TreeNode root2) {
-        if(root1 == null || root2 == null) {
-            return false;
+        if(root2 == null) {
+            return true;
         }
-        if(root1.val != root2.val) {
+        if(root1 == null) {
+            System.out.println("root1 is null");
             return false;
         }
 
+
+        if(root1.val != root2.val) {
+            System.out.println("different val");
+            return false;
+        }
         return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
     }
 }
--- src/niuke/swordoffer/TreeNode.java
+++ src/niuke/swordoffer/TreeNode.java
@@ -1,5 +1,12 @@
 package niuke.swordoffer;
 
+import sun.awt.image.ImageWatched;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * test
  *
@@ -8,10 +15,129 @@ package niuke.swordoffer;
  */
 
 public class TreeNode {
+
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x) {
+    ArrayList<Integer> preOrderResult = new ArrayList<>();
+    ArrayList<Integer> inOrderResult = new ArrayList<>();
+    ArrayList<Integer> postOrderResult = new ArrayList<>();
+    ArrayList<Integer> levelOrderResult = new ArrayList<>();
+
+    public TreeNode(int x) {
         val = x;
     }
+
+    public static void main(String[] args) {
+        char[] chars = {1,2,3,4,5,'#',7,8};
+        TreeNode root = constructTree(chars);
+        System.out.println("preOrder:");
+        int[] res = root.levelOrder(root);
+        System.out.println("res length: " + res.length);
+        for (int i :
+                res) {
+            System.out.print(i + ",");
+        }
+    }
+
+    public int[] preOrder(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        System.out.println(root.val);
+        preOrderResult.add(root.val);
+        preOrder(root.left);
+        preOrder(root.right);
+        return preOrderResult.stream().mapToInt(Integer::intValue).toArray();
+    }
+    public int[] inOrder(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        inOrder(root.left);
+        System.out.println(root.val);
+        inOrderResult.add(root.val);
+        inOrder(root.right);
+        return inOrderResult.stream().mapToInt(Integer::intValue).toArray();
+    }
+    public int[] postOrder(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.val);
+        postOrderResult.add(root.val);
+        return postOrderResult.stream().mapToInt(Integer::intValue).toArray();
+    }
+    public int[] levelOrder(TreeNode root) {
+        Queue<TreeNode> queueFather = new LinkedList<>();
+        Queue<TreeNode> queueChild = new LinkedList<>();
+//        this.levelOrderResult = ;
+        ((LinkedList<TreeNode>) queueFather).add(root);
+        while (!queueFather.isEmpty()) {
+            TreeNode current = ((LinkedList<TreeNode>) queueFather).removeFirst();
+            levelOrderResult.add(current.val);
+            if(current.left != null) {
+                queueChild.add(current.left);
+            }
+            if(current.right != null) {
+                queueChild.add(current.right);
+            }
+            if(queueFather.isEmpty()){
+                queueFather = queueChild;
+                queueChild = new LinkedList<>();
+            }
+        }
+        return levelOrderResult.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static TreeNode constructTree(char[] chars){
+        TreeNode root = new TreeNode(chars[0]);
+//        Queue<TreeNode> queueFather = new LinkedList<>();
+        Queue<TreeNode> queueFather = new LinkedList<>();
+        Queue<TreeNode> queueChild = new LinkedList<>();
+        queueFather.add(root);
+        int current = 1;
+        while (current < chars.length){
+            while (!queueFather.isEmpty()) {
+                TreeNode currentNode = ((LinkedList<TreeNode>) queueFather).removeFirst();
+                if(current < chars.length){
+                    TreeNode left;
+                    if(chars[current] == '#'){
+                        left = null;
+                    } else {
+                        left = new TreeNode(chars[current]);
+                    }
+                    queueChild.add(left);
+                    currentNode.left = left;
+                    current += 1;
+//                    System.out.println("left.val: " + left.val);
+//                    System.out.println("size:" + queueChild.size());
+                } else {
+                    break;
+                }
+
+
+//                System.out.println(current);
+                if(current < chars.length){
+                    TreeNode right;
+                    if(chars[current] == '#'){
+                        right = null;
+                    } else {
+                        right = new TreeNode(chars[current]);
+                    }
+                    queueChild.add(right);
+                    currentNode.right = right;
+                    current += 1;
+                } else {
+                    break;
+                }
+            }
+            queueFather = (LinkedList<TreeNode>)queueChild;
+            queueChild = new LinkedList<>();
+        }
+        return root;
+    }
+
 }
